6d3aaa6e496ea3fac0573905d0c89fdbbff236eb
orkunkilic <orkun.kilic@boun.edu.tr>
2020-06-27 15:21:59 +0300

    Passport Facebook OAuth

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '673475826541932',
+        clientSecret: 'fb5024e83bc03b9423b6b71176ed9d59'
+    }
 }
